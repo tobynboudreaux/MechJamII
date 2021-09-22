c7855c6aa117d5ac97e23ba52564c80d9469e6a2
tobynboudreaux <tobynboudreaux@gmail.com>
2021-09-22 16:12:43 -0500

    added basic HUD and projectile

--- Player/Player.gd
+++ Player/Player.gd
@@ -16,16 +16,27 @@ var rotation_helper
 
 var MOUSE_SENS = 0.05
 
+var p = preload("res://Projectile/Projectile.tscn").instance()
+onready var fire_timer = get_node("FireTimer")
+const DEFAULT_MAG_SIZE = 7
+var mag_size
+
 func _ready():
 	camera = $Camera
 	rotation_helper = $Rotation_Helper
 	
-func _physics_process(delta):
-	process_input(delta)
-	process_movement(delta)
-	process_camera_rotation(delta)
+	# ----------------------------------
+	# Ammunition SET
+	mag_size = DEFAULT_MAG_SIZE
+	p.set_vars(10, Vector3.DOWN * 20)
 	
-func process_input(delta):
+func _physics_process(delta):
+	process_input()
+	process_movement(delta)
+	process_camera_rotation()
+	process_fire()
+	
+func process_input():
 	
 	# ----------------------------------
 	# Walking
@@ -56,6 +67,12 @@ func process_input(delta):
 		if Input.is_action_just_pressed("jump"):
 			vel.y = JUMP_SPEED
 	# ----------------------------------
+	
+	# ----------------------------------
+	# Reload
+	if Input.is_action_just_pressed("reload"):
+		mag_size = DEFAULT_MAG_SIZE
+		$HUD.update_ammo_val(str(mag_size) + "/" + str(DEFAULT_MAG_SIZE))
 		
 func process_movement(delta):
 	dir.y = 0
@@ -80,9 +97,18 @@ func process_movement(delta):
 	vel.z = hvel.z
 	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
 
-func process_camera_rotation(delta):
+func process_camera_rotation():
 	var offset = -PI * 0.5
 	var screen_pos = camera.unproject_position(rotation_helper.global_transform.origin)
 	var mouse_pos = get_viewport().get_mouse_position()
 	var angle = screen_pos.angle_to_point(mouse_pos)
 	rotation_helper.rotation.y = -angle + offset
+
+func process_fire():
+	if Input.is_action_just_pressed("fire") && mag_size > 0 && fire_timer.time_left == 0:
+		owner.add_child(p)
+		p.transform = $Rotation_Helper/Model/MeshInstance/WeaponMuzzle.global_transform
+		p.velocity = -p.transform.basis.z * p.muzzle_velocity
+		mag_size -= 1
+		$HUD.update_ammo_val(str(mag_size) + "/" + str(DEFAULT_MAG_SIZE))
+		fire_timer.start()
